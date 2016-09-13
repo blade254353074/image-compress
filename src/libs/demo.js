48a846e7633add2d9e1f5c5c8a63fdef94c3a389
blade254353074 <blade254353074@hotmail.com>
2016-09-14 01:15:03 +0800

    Change js format

--- src/libs/demo.js
+++ src/libs/demo.js
@@ -1,6 +1,95 @@
 function $ (selector) {
   return document.querySelector(selector)
 }
+// init 
+window.URL = window.URL || window.webkitURL
+
+function newBlob (data, datatype) {
+  var out
+  try {
+    out = new Blob([data], { type: datatype })
+  } catch (e) {
+    window.BlobBuilder = window.BlobBuilder ||
+    window.WebKitBlobBuilder ||
+    window.MozBlobBuilder ||
+    window.MSBlobBuilder
+
+    if (e.name == 'TypeError' && window.BlobBuilder) {
+      var bb = new BlobBuilder()
+      bb.append(data)
+      out = bb.getBlob(datatype)
+    } else if (e.name == 'InvalidStateError') {
+      out = new Blob([data], { type: datatype })
+    }
+  }
+  return out
+}
+
+function isCanvasBlank (canvas) {
+  var blank = document.createElement('canvas')
+  blank.width = canvas.width
+  blank.height = canvas.height
+
+  return canvas.toDataURL() == blank.toDataURL()
+}
+
+function newBlob (data, datatype) {
+  var out
+  try {
+    out = new Blob([data], { type: datatype })
+  } catch (e) {
+    window.BlobBuilder = window.BlobBuilder ||
+    window.WebKitBlobBuilder ||
+    window.MozBlobBuilder ||
+    window.MSBlobBuilder
+
+    if (e.name == 'TypeError' && window.BlobBuilder) {
+      var bb = new BlobBuilder()
+      bb.append(data)
+      out = bb.getBlob(datatype)
+    } else if (e.name == 'InvalidStateError') {
+      out = new Blob([data], { type: datatype })
+    }
+  }
+  return out
+}
+
+function dataURL2Blob (dataURI) {
+  var byteStr
+  var intArray
+  var ab
+  var i
+  var mimetype
+  var parts
+
+  parts = dataURI.split(',')
+
+  if (~parts[0].indexOf('base64')) {
+    byteStr = atob(parts[1])
+  } else {
+    byteStr = decodeURIComponent(parts[1])
+  }
+
+  ab = new ArrayBuffer(byteStr.length)
+  intArray = new Uint8Array(ab)
+
+  for (i = 0; i < byteStr.length; i++) {
+    intArray[i] = byteStr.charCodeAt(i)
+  }
+
+  mimetype = parts[0].split(':')[1].split(';')[0]
+
+  return new Blob([ab], {
+    type: mimetype
+  })
+}
+
+function string2ArrayBuffer (string) {
+  var bytes = Array.prototype.map.call(string, function (c) {
+    return c.charCodeAt(0) & 0xff
+  })
+  return new Uint8Array(bytes).buffer
+}
 
 (function () {
   var file
@@ -12,96 +101,6 @@ function $ (selector) {
   var pureBase64ImageData // 不包含 /^data:image\/(.+);base64,/ 的 base64 字符串
   var binaryString // atob 转码后的 二进制文本 
   var boundaryString // 构造为 multipart 的文本
-
-  // init 
-  window.URL = window.URL || window.webkitURL
-
-  function newBlob (data, datatype) {
-    var out
-    try {
-      out = new Blob([data], { type: datatype })
-    } catch (e) {
-      window.BlobBuilder = window.BlobBuilder ||
-      window.WebKitBlobBuilder ||
-      window.MozBlobBuilder ||
-      window.MSBlobBuilder
-
-      if (e.name == 'TypeError' && window.BlobBuilder) {
-        var bb = new BlobBuilder()
-        bb.append(data)
-        out = bb.getBlob(datatype)
-      } else if (e.name == 'InvalidStateError') {
-        out = new Blob([data], { type: datatype })
-      }
-    }
-    return out
-  }
-
-  function isCanvasBlank (canvas) {
-    var blank = document.createElement('canvas')
-    blank.width = canvas.width
-    blank.height = canvas.height
-
-    return canvas.toDataURL() == blank.toDataURL()
-  }
-
-  function newBlob (data, datatype) {
-    var out
-    try {
-      out = new Blob([data], { type: datatype })
-    } catch (e) {
-      window.BlobBuilder = window.BlobBuilder ||
-      window.WebKitBlobBuilder ||
-      window.MozBlobBuilder ||
-      window.MSBlobBuilder
-
-      if (e.name == 'TypeError' && window.BlobBuilder) {
-        var bb = new BlobBuilder()
-        bb.append(data)
-        out = bb.getBlob(datatype)
-      } else if (e.name == 'InvalidStateError') {
-        out = new Blob([data], { type: datatype })
-      }
-    }
-    return out
-  }
-
-  function dataURL2Blob (dataURI) {
-    var byteStr
-    var intArray
-    var ab
-    var i
-    var mimetype
-    var parts
-
-    parts = dataURI.split(',')
-
-    if (~parts[0].indexOf('base64')) {
-      byteStr = atob(parts[1])
-    } else {
-      byteStr = decodeURIComponent(parts[1])
-    }
-
-    ab = new ArrayBuffer(byteStr.length)
-    intArray = new Uint8Array(ab)
-
-    for (i = 0; i < byteStr.length; i++) {
-      intArray[i] = byteStr.charCodeAt(i)
-    }
-
-    mimetype = parts[0].split(':')[1].split(';')[0]
-
-    return new Blob([ab], {
-      type: mimetype
-    })
-  }
-
-  function string2ArrayBuffer (string) {
-    var bytes = Array.prototype.map.call(string, function (c) {
-      return c.charCodeAt(0) & 0xff
-    })
-    return new Uint8Array(bytes).buffer
-  }
 
   // file on change
   J_File.addEventListener('change', function (e) {
@@ -184,7 +183,6 @@ function $ (selector) {
     var mimeType = fileType || 'image/png'
     var quality = 30
     var compressedBlob
-
 
     compressedImageDataURL = canvas.toDataURL(mimeType, quality / 100)
     compressedBlob = dataURL2Blob(compressedImageDataURL)
